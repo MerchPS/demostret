66e028b4943c91a47f6a888ef11a220c9178262b
MerchPS <merchpsbackup@gmail.com>
2025-05-18 13:32:38 +0700

    Update save_user.js

--- api/save_user.js
+++ api/save_user.js
@@ -2,7 +2,6 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = "mongodb+srv://HansDB:Hansmoses2007%23@lisensi.98zue9l.mongodb.net/?retryWrites=true&w=majority&appName=Lisensi";
 
-// Mongo client setup
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -12,15 +11,14 @@ const client = new MongoClient(uri, {
 });
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     try {
       await client.connect();
-      const db = client.db("lisensidb"); // Ubah ke nama database kamu
-      const collection = db.collection("users");
+      const db = client.db("lisensidb"); // Ganti sesuai nama database kamu
+      const collection = db.collection("users"); // Ganti nama collection kalau perlu
 
       const data = req.body;
 
-      // Simpan data ke MongoDB
       const result = await collection.insertOne(data);
 
       res.status(200).json({ success: true, insertedId: result.insertedId });
